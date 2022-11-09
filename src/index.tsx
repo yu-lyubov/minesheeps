c4022c265c4a11b2049c6e528125cdb314e97bde
yu-lyubov <lyuba.yureva.99@mail.ru>
2022-11-09 23:42:06 +0300

    delete files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { Flip, ToastContainer } from 'react-toastify';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { store } from './app/store';
 import 'react-toastify/dist/ReactToastify.css';
@@ -30,8 +29,3 @@ root.render(
   </Provider>
 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
